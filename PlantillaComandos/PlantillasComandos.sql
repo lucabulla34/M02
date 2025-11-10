2c9aeb169088ff3ac4070f3eccf3ddcca7e83522
lucabulla34 <luca.bulla.7e9@itb.cat>
2025-11-10 09:50:35 +0100

    Added EA3 and updated somethings

--- PlantillaComandos/PlantillasComandos.sql
+++ PlantillaComandos/PlantillasComandos.sql
@@ -9,17 +9,18 @@ psql -U bbdd -W -d bbdd -h localhost < "ruta"
 */
 
 /* CREACIÓ D'USUARIS
-Create role usuarioDatabase
 superuser
 createdb
 createrole
 inherit
 login
 replication;
-ALTER user usuarioDatabase with password 'usuarioDatabase' ;
-CREATE database usuarioDatabase with owner usuarioDatabase;
-CREATE SCHEMA usuarioDatabase;
-GRANT ALL PRIVILEGES ON DATABASE usuarioDatabase TO usuarioDatabase;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA usuarioDatabase TO usuarioDatabase;
-\c usuarioDatabase;
+ALTER user shop with password 'shop' ;
+CREATE database shop with owner shop;
+CREATE SCHEMA shop;
+GRANT ALL PRIVILEGES ON DATABASE shop TO shop;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA shop TO shop;
+\q
+psql shop -W -d shop
+\c shop;
 */
